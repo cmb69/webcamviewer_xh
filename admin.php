645fc9b5089accdf4b154e184a84a6d3e7498e50
cmb69 <cmbecker69@gmx.de>
2013-04-28 20:34:24 +0000

    refactored plugin info as view

--- admin.php
+++ admin.php
@@ -12,67 +12,53 @@ if (!defined('CMSIMPLE_XH_VERSION')) {
     exit;
 }
 
+function Webcamviewer_systemChecks() // RELEASE-TODO
+{
+    global $pth, $tx, $plugin_tx;
 
-/**
- * Returns the plugin version information view.
- *
- * @return string  The (X)HTML.
- */
-function webcamviewer_version() {
-    global $pth;
-
-    return '<h1><a href="http://3-magi.net/?CMSimple_XH/Webcamviewer_XH">Webcamviewer_XH</a></h1>'."\n"
-	    .tag('img src="'.$pth['folder']['plugins'].'webcamviewer/webcamviewer.png" class="webcamviewer_plugin_icon"')
-	    .'<p style="margin-top: 2em">Version: '.WEBCAMVIEWER_VERSION.'</p>'."\n"
-	    .'<p>Copyright &copy; 2012 <a href="http://3-magi.net">Christoph M. Becker</a></p>'."\n"
-	    .'<p class="webcamviewer_license">This program is free software: you can redistribute it and/or modify'
-	    .' it under the terms of the GNU General Public License as published by'
-	    .' the Free Software Foundation, either version 3 of the License, or'
-	    .' (at your option) any later version.</p>'."\n"
-	    .'<p class="webcamviewer_license">This program is distributed in the hope that it will be useful,'
-	    .' but WITHOUT ANY WARRANTY; without even the implied warranty of'
-	    .' MERCHAN&shy;TABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the'
-	    .' GNU General Public License for more details.</p>'."\n"
-	    .'<p class="webcamviewer_license">You should have received a copy of the GNU General Public License'
-	    .' along with this program.  If not, see'
-	    .' <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.</p>'."\n";
+    $ptx = $plugin_tx['webcamviewer'];
+    $phpVersion = '4.0.7';
+    $checks = array();
+    $checks[sprintf($ptx['syscheck_phpversion'], $phpVersion)] =
+	version_compare(PHP_VERSION, $phpVersion) >= 0 ? 'ok' : 'fail';
+    foreach (array() as $ext) {
+	$checks[sprintf($ptx['syscheck_extension'], $ext)] =
+	    extension_loaded($ext) ? 'ok' : 'fail';
+    }
+    $checks[$ptx['syscheck_magic_quotes']] =
+	!get_magic_quotes_runtime() ? 'ok' : 'fail';
+    $checks[$ptx['syscheck_encoding']] =
+	strtoupper($tx['meta']['codepage']) == 'UTF-8' ? 'ok' : 'warn';
+    $folders = array();
+    foreach (array('config/', 'languages/') as $folder) {
+	$folders[] = $pth['folder']['plugins'] . 'webcamviewer/' . $folder;
+    }
+    foreach ($folders as $folder) {
+	$checks[sprintf($ptx['syscheck_writable'], $folder)] =
+	    is_writable($folder) ? 'ok' : 'warn';
+    }
+    return $checks;
 }
 
 
-/**
- * Returns the requirements information view.
- *
- * @return string  The (X)HTML.
- */
-function webcamviewer_system_check() { // RELEASE-TODO
-    global $pth, $tx, $plugin_tx;
+function Webcamviewer_info()
+{
+    global $pth, $plugin_tx;
 
-    define('WEBCAMVIEWER_PHP_VERSION', '4.0.7');
     $ptx = $plugin_tx['webcamviewer'];
-    $imgdir = $pth['folder']['plugins'].'webcamviewer/images/';
-    $ok = tag('img src="'.$imgdir.'ok.png" alt="ok"');
-    $warn = tag('img src="'.$imgdir.'warn.png" alt="warning"');
-    $fail = tag('img src="'.$imgdir.'fail.png" alt="failure"');
-    $o = '<h4>'.$ptx['syscheck_title'].'</h4>'
-	    .(version_compare(PHP_VERSION, WEBCAMVIEWER_PHP_VERSION) >= 0 ? $ok : $fail)
-	    .'&nbsp;&nbsp;'.sprintf($ptx['syscheck_phpversion'], WEBCAMVIEWER_PHP_VERSION)
-	    .tag('br')."\n";
-    foreach (array('gd') as $ext) {
-	$o .= (extension_loaded($ext) ? $ok : $fail)
-		.'&nbsp;&nbsp;'.sprintf($ptx['syscheck_extension'], $ext).tag('br')."\n";
+    $labels = array(
+	'syscheck' => $ptx['syscheck_title'],
+	'about' => $ptx['about']
+    );
+    foreach (array('ok', 'warn', 'fail') as $state) {
+	$images[$state] = $pth['folder']['plugins']
+	    . 'webcamviewer/images/' . $state . '.png';
     }
-    $o .= (!get_magic_quotes_runtime() ? $ok : $fail)
-	    .'&nbsp;&nbsp;'.$ptx['syscheck_magic_quotes'].tag('br')."\n";
-    $o .= tag('br').(strtoupper($tx['meta']['codepage']) == 'UTF-8' ? $ok : $warn)
-	    .'&nbsp;&nbsp;'.$ptx['syscheck_encoding'].tag('br')."\n";
-    $o .= (file_exists($pth['folder']['plugins'].'jquery/jquery.inc.php') ? $ok : $fail)
-	    .'&nbsp;&nbsp;'.$ptx['syscheck_jquery'].tag('br').tag('br')."\n";
-    foreach (array('config/', 'css/', 'languages/') as $folder) {
-	$folder = $pth['folder']['plugins'].'webcamviewer/'.$folder;
-	$o .= (is_writable($folder) ? $ok : $warn)
-		.'&nbsp;&nbsp;'.sprintf($ptx['syscheck_writable'], $folder).tag('br')."\n";
-    }
-    return $o;
+    $checks = Webcamviewer_systemChecks();
+    $icon = $pth['folder']['plugins'] . 'webcamviewer/webcamviewer.png';
+    $version = WEBCAMVIEWER_VERSION;
+    $bag = compact('labels', 'images', 'checks', 'icon', 'version');
+    return Webcamviewer_render('info', $bag);
 }
 
 
@@ -83,7 +69,7 @@ if (!empty($webcamviewer)) {
     $o .= print_plugin_admin('off');
     switch ($admin) {
 	case '':
-	    $o .= webcamviewer_version().tag('hr').webcamviewer_system_check();
+	    $o .= Webcamviewer_info();
 	    break;
 	default:
 	    $o .= plugin_admin_common($action, $admin, $plugin);
--- languages/en.php
+++ languages/en.php
@@ -5,8 +5,9 @@ $plugin_tx['webcamviewer']['syscheck_phpversion']="PHP version &ge; %s";
 $plugin_tx['webcamviewer']['syscheck_extension']="Extension '%s' loaded";
 $plugin_tx['webcamviewer']['syscheck_encoding']="Encoding 'UTF-8' configured";
 $plugin_tx['webcamviewer']['syscheck_magic_quotes']="Magic quotes runtime off";
-$plugin_tx['webcamviewer']['syscheck_jquery']="<a href=\"http://www.cmsimple-xh.com/wiki/doku.php/plugins:jquery4cmsimple\">jQuery4CMSimple plugin</a> installed";
 $plugin_tx['webcamviewer']['syscheck_writable']="Folder '%s' writable";
+
+$plugin_tx['webcamviewer']['about']="About Webcamviewer_XH";
 
 $plugin_tx['webcamviewer']['cf_autoload']="Whether the webcamviewer should be automatically enabled on all pages: \"1\" (yes) or \"0\" (no).";
 $plugin_tx['webcamviewer']['cf_interval']="The duration in milliseconds between two reloads.";
