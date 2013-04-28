748f54f5c95b5e78ba6efa17434ad01bd34a2460
cmb69 <cmbecker69@gmx.de>
2013-04-28 20:20:11 +0000

    removed config option folder_images

--- config/config.php
+++ config/config.php
@@ -1,7 +1,6 @@
 <?php
 
-$plugin_cf['webcamviewer']['folder_images']="images/webcam/";
 $plugin_cf['webcamviewer']['autoload']="0";
-$plugin_cf['webcamviewer']['interval']="60000";
+$plugin_cf['webcamviewer']['interval']="2000";
 
 ?>
--- languages/en.php
+++ languages/en.php
@@ -8,7 +8,6 @@ $plugin_tx['webcamviewer']['syscheck_magic_quotes']="Magic quotes runtime off";
 $plugin_tx['webcamviewer']['syscheck_jquery']="<a href=\"http://www.cmsimple-xh.com/wiki/doku.php/plugins:jquery4cmsimple\">jQuery4CMSimple plugin</a> installed";
 $plugin_tx['webcamviewer']['syscheck_writable']="Folder '%s' writable";
 
-$plugin_tx['webcamviewer']['cf_folder_images']="Path to a folder relative to the CMSimple root directory, where the webcam images will be placed. E.g. \"images/webcam/\".";
 $plugin_tx['webcamviewer']['cf_autoload']="Whether the webcamviewer should be automatically enabled on all pages: \"1\" (yes) or \"0\" (no).";
 $plugin_tx['webcamviewer']['cf_interval']="The duration in milliseconds between two reloads.";
 
