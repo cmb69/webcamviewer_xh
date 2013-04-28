15424a232ffbe3eeb6d60d189de51ed70ad16dca
cmb69 <cmbecker69@gmx.de>
2013-04-28 20:58:11 +0000

    cleaned up index.php

--- index.php
+++ index.php
@@ -1,21 +1,43 @@
 <?php
 
 /**
- * Front-End of Webcamviewer_XH.
+ * Index of Webcamviewer_XH.
  *
- * Copyright (c) 2012 Christoph M. Becker (see license.txt)
+ * @package    Webcamviewer
+ * @copyright  Copyright (c) 2012-2013 Christoph M. Becker <http://3-magi.net/>
+ * @license    http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version    $Id$
+ * @link       http://3-magi.net/?CMSimple_XH/Webcamviewer_XH
  */
 
 
+/*
+ * Prevent direct access.
+ */
 if (!defined('CMSIMPLE_XH_VERSION')) {
     header('HTTP/1.0 403 Forbidden');
     exit;
 }
 
 
+/**
+ * The webcamviewer controller class.
+ */
+require $pth['folder']['plugin_classes'] . 'controller.php';
+
+
+/**
+ * The plugin version.
+ */
 define('WEBCAMVIEWER_VERSION', '1beta1');
 
 
+/**
+ * Initializes the webcamviewer.
+ *
+ * @global object  The webcamviewer controller.
+ * @return string  The (X)HTML.
+ */
 function webcamviewer()
 {
     global $_Webcamviewer;
@@ -24,8 +46,9 @@ function webcamviewer()
 }
 
 
-require $pth['folder']['plugin_classes'] . 'controller.php';
-
+/**
+ * Create the webcamviewer controller.
+ */
 $_Webcamviewer = new Webcamviewer_Controller();
 
 ?>
