9dcd72f82a392b6c5fb728018df0dad767c4763d
cmb69 <cmbecker69@gmx.de>
2013-04-28 21:56:37 +0000

    added German localization

---
+++ languages/de.php
@@ -0,0 +1,15 @@
+<?php
+
+$plugin_tx['webcamviewer']['syscheck_title']="System-Prüfung";
+$plugin_tx['webcamviewer']['syscheck_phpversion']="PHP-Version &ge; %s";
+$plugin_tx['webcamviewer']['syscheck_extension']="Erweiterung '%s' geladen";
+$plugin_tx['webcamviewer']['syscheck_encoding']="'UTF-8' Kodierung eingestellt";
+$plugin_tx['webcamviewer']['syscheck_magic_quotes']="Magic quotes runtime off";
+$plugin_tx['webcamviewer']['syscheck_writable']="Ordner '%s' schreibbar";
+
+$plugin_tx['webcamviewer']['about']="Über Webcamviewer_XH";
+
+$plugin_tx['webcamviewer']['cf_autoload']="Ob der Webcamviewer automatisch auf allen Seiten aktiviert sein soll: \"1\" (ja) oder \"0\" (nein).";
+$plugin_tx['webcamviewer']['cf_interval']="Das Intervall in Millisekunden zwischen zwei Bild-Aktualisierungen.";
+
+?>
