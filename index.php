ead21c1f5e0a82412c94390b0579d1e36e384829
Christoph M. Becker <cmbecker69@gmx.de>
2015-03-04 23:50:33 +0100

    removed SVN Id keywords

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
  * @package    Webcamviewer
  * @copyright  Copyright (c) 2012-2013 Christoph M. Becker <http://3-magi.net/>
  * @license    http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
- * @version    $Id$
  * @link       http://3-magi.net/?CMSimple_XH/Webcamviewer_XH
  */
 
