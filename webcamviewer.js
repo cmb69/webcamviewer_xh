e3b0f5cfa5a8b2a611857b5044f4624e24f5e401
Christoph M. Becker <cmbecker69@gmx.de>
2022-03-26 16:42:34 +0100

    Switch to GNU-style file headers

--- webcamviewer.js
+++ webcamviewer.js
@@ -1,8 +1,20 @@
-/*!
- * Webcamviewer_XH
+/**
+ * Copyright 2012-2022 Christoph M. Becker
  *
- * @author  Christoph M. Becker <cmbecker69@gmx.de>
- * @license GPL-3.0+
+ * This file is part of Webcamviewer_XH.
+ *
+ * Webcamviewer_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Webcamviewer_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Webcamviewer_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*jslint browser: true, maxlen: 80 */
